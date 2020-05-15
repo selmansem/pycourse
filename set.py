ecfac4c4eb42f42a71454ebcdfda78bb0c7e4e2d
Selman <33104288+selmansem@users.noreply.github.com>
2020-05-15 10:41:51 +0200

    Python Course Examples

--- set.py
+++ set.py
@@ -1,11 +1,7 @@
 # import only system from os
 from os import system, name
 
-# import sleep to show output for some time period
-from time import sleep
-
 # define our clear function
-
 def clear():
 
     # for windows
