6ac292888da215b28a2ea2a50dbc1dce43179539
Selman <33104288+selmansem@users.noreply.github.com>
2020-05-15 11:06:03 +0200

    Python Course Examples

--- dictionaries.py
+++ dictionaries.py
@@ -20,3 +20,11 @@ clear()
 #######################################################################
 
 
+cities = {
+    "Madrid": (-3.7025600, 40.4165000),  # Long and lat of Madrid
+    "Sevilla": (-5.9731700, 37.3828300),  # Long and lat of Sevilla
+    "Granada": (-3.6066700, 37.1881700),  # Long and lat of Granada
+    "Barcelona": (2.1589900, 41.3887900)  # Long and lat of Barcelona
+}
+
+print(cities.get("Madrid"))
